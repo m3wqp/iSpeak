bf43b9e17a0f18add843e5820b77d8e9dee0c48f
Влад Руцких <m3wqp@mail.ru>
2022-04-13 17:27:31 +0300

    update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,10 +7,6 @@ import {login, logout} from "../../state/reducers/auth-Reducer";
 import {Redirect} from "react-router-dom";
 import style from "./Login.module.css"
 
-/*<div>
-<div>Логин: m3wqp@mail.ru</div>
-<div>Пароль: 123qwe</div>
-</div>*/
 
 const maxLength10 = maxLengthCreator(30)
 
@@ -19,15 +15,16 @@ const LoginForm = ({handleSubmit, error, captchaUrl}) => {
   return (
     <form className={style.formContainer} onSubmit={handleSubmit}>
 
+
       <div className={style.formLoginContent}>
-        <h2 style={{marginTop: "1em",marginLeft:'3em'}}>Логин</h2>
+        <h2 style={{marginTop: "1em", marginLeft: '3em'}}>Логин</h2>
         <div className={style.fieldContent}>
           <div>
-            <div style={{marginTop: "1em",marginLeft:'2em'}}>Логин</div>
+            <div style={{marginTop: "1em", marginLeft: '2em'}}>Логин</div>
             {createField("Логин", "email", InputControl, [required, maxLength10], {}, '', 'm3wqp')}
           </div>
           <div>
-            <div style={{marginTop: "1em",marginLeft:'2em'}}>Пароль</div>
+            <div style={{marginTop: "1em", marginLeft: '2em'}}>Пароль</div>
             {createField("Пароль", "password", InputControl, [required, maxLength10], {
               type: "password",
               value: '123qwe'
@@ -35,25 +32,29 @@ const LoginForm = ({handleSubmit, error, captchaUrl}) => {
           </div>
         </div>
 
-        <div style={{marginTop: "1em",marginLeft:'2em'}} className={style.checkbox}>
+        <div style={{marginTop: "1em", marginLeft: '2em'}} className={style.checkbox}>
           {createField(null, "rememberMe", InputControl, null, {type: "checkbox"}, "Запомнить меня")}
         </div>
 
 
-          {captchaUrl && <img src={captchaUrl}/>}
-          {captchaUrl && createField("symbols from image", "captcha", InputControl, [required, maxLength10],)}
-
+        {captchaUrl && <img src={captchaUrl}/>}
+        {captchaUrl && createField("symbols from image", "captcha", InputControl, [required, maxLength10],)}
 
 
         {error && <div className={style.formError}>
           <span>{error}</span>
         </div>}
         <div>
-          <button  style={{marginTop: "1em",marginLeft:'3em'}} className={style.btn}>
+          <button style={{marginTop: "1em", marginLeft: '3em'}} className={style.btn}>
             Отправить
           </button>
         </div>
+        <div>
+          <div>Логин: m3wqp@mail.ru</div>
+          <div>Пароль: 123qwe</div>
+        </div>
       </div>
+
 
     </form>
   )
